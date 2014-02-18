23180bccd39bf178983901b2560cc4a40852f6c9
sliperro <sliperro@gmail.com>
2014-02-18 23:24:03 +0100

    Chapter 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Test1::Application.routes.draw do
+  resources :events
+
   resources :tickets
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217223932) do
+ActiveRecord::Schema.define(version: 20140218221453) do
+
+  create_table "events", force: true do |t|
+    t.string   "artists"
+    t.text     "description"
+    t.decimal  "price_low"
+    t.decimal  "price_high"
+    t.date     "event_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tickets", force: true do |t|
     t.string   "name"
@@ -21,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140217223932) do
     t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
   end
 
 end
